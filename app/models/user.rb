a5886929c6ede95a676b301bf6aa4282abb72cd0
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-08-05 23:57:11 -0600

    going to bed, allow top level params

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :password
+  validates_presence_of :password, presence: true, confirmation: true
 
   has_secure_password
 end
